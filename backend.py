65e6424eb5b4aa8cd10b95842364aa1a6105bd86
alexconecko <89869524+alexconecko@users.noreply.github.com>
2022-11-23 16:29:26 +1100

    cry

--- backend.py
+++ backend.py
@@ -1,14 +1,75 @@
 
 class BackEndManager:
+    graphics_cards_inventory = []
     
-    def load_data_from_file(self):
-        pass
+    def load_data_from_file(self, file_name:str)->str:
+        try:
+            data_file = open(file_name, "r")
+        except:
+            raise ValueError("Unable to open file: " + file_name)
+
+        line = data_file.readline()
+        while line != "":
+            fields = line.strip().split(",")
+            if len(fields) != 3:
+                raise ValueError("Incorrect number of values on line: " + line)
+
+            try:
+                card_name = fields[0]
+                stock_amount = fields[1]
+                card_price = fields[2]
+            except:
+                raise ValueError("Incorrectly formatted fata line in file: " + line)
+            
         
             
 class Graphics_Card():
-    card_name = ""
-    stock_amount = 0
-    card_price = 0.00
+    __card_name = ""
+    __stock_amount = 0
+    __card_price = 0.00
 
-    def __init__(self):
-        pass
+    #constructor created to initialise every new object and ensure the
+    #arguments are provided as required.
+    def __init__(self, card_name, stock_amount, card_price):
+        self.__card_name = card_name
+        self.__stock_amount = stock_amount
+        self.__card_price = card_price
+    
+    #property tag is used for our getter methods, using this tag we can call the methods 
+    #without the use of () at the end providing the illusion of an actual attribute/property.     
+    @property
+    def card_name(self):
+        return self.__card_name
+    
+    @property
+    def stock_amount(self):
+        return self.__stock_amount
+    
+    @property
+    def card_price(self):
+        return self.__card_price
+    
+    #Just as an accessor/getter method allows the retrieval of a property of an object, 
+    # a mutator/setter method allows the modification of a property, by creating a setter
+    #method for each property above we fix attribute errors
+    @card_name.setter
+    def card_name(self, value):
+        #if statement used to validate that the value is not an empty string
+        if value != 0:
+            self.__card_name = value
+        else:
+            raise ValueError("item name cannot be empty.")
+    
+    @stock_amount.setter
+    def stock_amount(self, value):
+        if value != 0:
+            self.__stock_amount = value
+        else:
+            raise ValueError("item servings cannot be empty.")
+            
+    @card_price.setter
+    def card_price(self, value):
+        if value != 0:
+            self.__card_price = value
+        else:
+            raise ValueError("item price cannot be empty.")
--- frontend.py
+++ frontend.py
@@ -1,13 +1,14 @@
 import sys
+import backend
 
 class FrontEndUI():
 
     #constructor needs to be created with __frontend as a parameter
     #to avoid a TypeError due to an argument being passed when instantiating
     #the class within application.py
-    def __init__(self, __frontend):
-        self.frontend = __frontend
-        
+    def __init__(self, __backend):
+        self.__backend = backend.BackEndManager()
+
     def show_ui(self):
         menu = "\n===============================\n"
         menu += "Graphics Card Inventory Manager\n"
@@ -19,68 +20,62 @@ class FrontEndUI():
         sys.stdout.write(menu)
         
         menu = menu.lower()
-        choice = User_Input_Handling.get_str(self, "Enter choice: ").lower()
+        choice = get_str("Enter choice: ").lower()
         while not "[" + choice + "]" in menu:
-            choice = User_Input_Handling.get_str(self, (choice + " was an invalid choice! Re-enter: ")).lower()
+            choice = get_str((choice + " was an invalid choice! Re-enter: ")).lower()
         
         return choice
     
-#this class has been created to hold the methods which deal with 
-#taking user inputs, validating them and then returning those values
-#so they can be later processed using methods from the backend operations class    
-class User_Input_Handling():
-    #this method ensures that user inputs are not blank, and if so the user
-    #is prompted to re-enter their input to ensure there is a valid string.
-    #You can accomplish the same thing using just an if statement after readlines
-    #however creating this method significantly reduces code duplication as we are 
-    #constantly needing strings from the user.
-    def get_str(self, prompt):
-        sys.stdout.write(prompt)
+def get_str(prompt:str)->str:
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+    value = sys.stdin.readline().strip()
+    #while the length of the string is 0 - or blank, the loop will repeat only
+    #breaking when their is at least one character in the string.
+    while len(value) == 0:
+        sys.stdout.write("Input cannot be blank. Re-enter: ")
         sys.stdout.flush()
         value = sys.stdin.readline().strip()
-        #while the length of the string is 0 - or blank, the loop will repeat only
-        #breaking when their is at least one character in the string.
-        while len(value) == 0:
-            sys.stdout.write("Input cannot be blank. Re-enter: ")
-            sys.stdout.flush()
-            value = sys.stdin.readline().strip()
-        return value
+    return value
     
-    #this method ensures strings are converted to floats when needed. this is a good
-    #time to talk about the parameters found in most methods of this class, of course there
-    #is self but also prompt, prompt is needed so we can output strings to direct the user
-    #before they provide input, this reduces code duplication
-    def get_float(self, prompt):
-        value = None
-        #gracefully handle errors using try and except during a while loop in
-        #in order to reduce code duplication
-        while value == None:
-            try:
-                value = float(User_Input_Handling.get_str(self, prompt))
-            except:
-                prompt = "That wasn't right. Re-enter: "
-        return value
+#this method ensures strings are converted to floats when needed. this is a good
+#time to talk about the parameters found in most methods of this class, of course there
+#is self but also prompt, prompt is needed so we can output strings to direct the user
+#before they provide input, this reduces code duplication
+def get_float(prompt:str)->str:
+    value = None
+    #gracefully handle errors using try and except during a while loop in
+    #in order to reduce code duplication
+    while value == None:
+        try:
+            value = float(get_str(prompt))
+        except:
+            prompt = "That wasn't right. Re-enter: "
+    return value
 
-    #this method ensures that the previous get float method is always a positive number.
-    def get_positive_float(self, prompt):
-        value = User_Input_Handling.get_float(self, prompt)
-        #this loop functions similarly to the loop in the get_float method however breaks 
-        #when the user inputs a positive number and repeats if its negative, this ensures
-        #the value we needed is returned  at the end.
-        while value < 0:
-            value = User_Input_Handling.get_float(self, "Input must be positive. Re-enter: ")
-        return value
+#this method ensures that the previous get float method is always a positive number.
+def get_positive_float(self, prompt:str)->str:
+    value = get_float(self, prompt)
+    #this loop functions similarly to the loop in the get_float method however breaks 
+    #when the user inputs a positive number and repeats if its negative, this ensures
+    #the value we needed is returned  at the end.
+    while value < 0:
+        value = get_float(self, "Input must be positive. Re-enter: ")
+    return value
     
     #this method ensures that strings are converted to integers when needed.
     #the while loop will only break when an integer is entered and will otherwise
     #continuously repeat. Like the other methods in this class, this could have been done 
     #differently using if statements however this implementation handles errors better, and further
     #assists us in reducing code duplication should the user want to add multiple records.
-    def get_int(self, prompt):
-        value = None
-        while value == None:
-            try:
-                value = int(User_Input_Handling.get_str(self, prompt))
-            except:
-                prompt = "That wasn't right. Re-enter: "
-        return value
+
+def get_int(self, prompt:str)->str:
+    value = None
+    while value == None:
+        try:
+            value = int(get_str(self, prompt))
+        except:
+            prompt = "That wasn't right. Re-enter: "
+    return value
+    
+#TODO: implement auto load and menu option to load another file.
