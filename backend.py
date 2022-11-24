344b964d9fa9b7fc251e68c54290f61771ecd607
alexconecko <89869524+alexconecko@users.noreply.github.com>
2022-11-24 15:20:02 +1100

    intial refactor

--- backend.py
+++ backend.py
@@ -1,7 +1,10 @@
 
 class BackEndManager:
     graphics_cards_inventory = []
-    data_file = ""
+    data_file = None
+    
+    def __init__(self):
+        self.data_file = BackEndManager.load_file(self, file_name="")
         
     @staticmethod
     def add_card(card_name, stock_amount, card_price):
@@ -10,6 +13,27 @@ class BackEndManager:
         gpu_item.stock_amount = stock_amount
         gpu_item.card_price = card_price
         BackEndManager.graphics_cards_inventory.append(gpu_item)
+        
+    def load_file(self, file_name:str)->str:
+        
+        try:
+            BackEndManager.data_file = open(file_name, "r")
+            line = BackEndManager.data_file.readline().strip()
+            while line != "":
+                fields = line.strip().split(",")
+                if len(fields) != 3:
+                    raise ValueError("Incorrect number of values on line: " + str(line))
+                else:
+                    card_name = fields[0]
+                    stock_amount = fields[1]
+                    card_price = fields[2]
+                    BackEndManager.add_card(card_name, stock_amount, card_price)
+        except FileNotFoundError:
+            pass
+            
+                
+            
+            
     
         
             
